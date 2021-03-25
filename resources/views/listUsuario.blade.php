343454bf7b7fd9e8e286b6cd1b2139245813c373
MatheusPm18 <matheusmelo@acad.ifma.edu.br>
2021-03-25 13:28:16 -0300

    tent1

---
+++ resources/views/listUsuario.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     
+ 
+
+</head>
+
+<body>
+<table class="table-secondary">
+    <thead>
+    <tr>
+    <tr scope="col">12</tr>
+    <tr scope="co1">23</tr>
+    <tr scope="col">34</tr>
+    <tr scope="col">45</tr>
+
+    </tr>
+    </thead>
+
+    <Tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>nao sei1</td>
+        <td>nao sei</td>
+        <td>nao sei</td>
+    </tr>
+ </table>
